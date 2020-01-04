6e973db48d1376fad1675b0e34bbc93b73141c06
Richard <wei.richard.s@gmail.com>
2020-01-04 14:49:10 -0800

    add react-rails and dependencies add react navbar

---
+++ app/javascript/components/MainNavbar.js
@@ -0,0 +1,29 @@
+import React from "react"
+// import PropTypes from "prop-types"
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import Form from 'react-bootstrap/Form'
+import FormControl from 'react-bootstrap/FormControl'
+import Button from 'react-bootstrap/Button'
+
+class MainNavbar extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+        <Navbar bg="dark" variant="dark">
+          <Navbar.Brand href="#home"><h2>Ticket Alert</h2></Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link href="#performers">Performers</Nav.Link>
+            <Nav.Link href="#venue">Venue</Nav.Link>
+          </Nav>
+          <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-info">Search</Button>
+          </Form>
+        </Navbar>
+      </React.Fragment>
+    );
+  }
+}
+
+export default MainNavbar
