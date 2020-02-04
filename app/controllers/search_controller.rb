3656aa8144c397ab08134ccf2f8df267ad2a5cf0
Richard <wei.richard.s@gmail.com>
2020-02-03 16:10:09 -0800

    limit ransack searches to 100 results

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,8 +2,8 @@ class SearchController < ApplicationController
  
   def index
     @q = params[:q]
-    @performers_search = Performer.ransack(name_cont: @q).limit(50)
-    @events_search = Event.ransack(name_cont: @q).limit(50)
+    @performers_search = Performer.limit(100).ransack(name_cont: @q)
+    @events_search = Event.limit(100).ransack(name_cont: @q)
     @performers_results = @performers_search.result
     @events_results = @events_search.result.order("event_time_utc ASC").
         as_json(include: [:performers => {:only=> [:id,:slug]}])
