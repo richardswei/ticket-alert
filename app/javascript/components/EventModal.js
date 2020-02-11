5724d8c9fb25d168aa3a7464fec720381b11b1ae
Richard <wei.richard.s@gmail.com>
2020-02-11 12:35:54 -0800

    try it without anything in it

--- app/javascript/components/EventModal.js
+++ app/javascript/components/EventModal.js
@@ -9,7 +9,7 @@ function getPriceTextFromList(price_list) {
 
 class EventModal extends Component {
   render() {
-    const event_details = this.props.event_details
+    // const event_details = this.props.event_details
     return (
       <Modal
         show={this.props.show}
@@ -18,40 +18,7 @@ class EventModal extends Component {
         aria-labelledby="contained-modal-title-vcenter"
         centered
       >
-        <Modal.Header closeButton>
-          <Modal.Title id="contained-modal-title-vcenter">
-            {event_details.name}
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Container>
-            <Row className="justify-content-md-center">
-              <Image
-                className="team-logo-modal"
-                src={`/logos/${event_details.performers.filter((x)=>x.slug!==event_details.home_team)[0].slug}.svg`}>
-              </Image>
-              <span className="centered-header"><h3>@</h3></span>
-              <Image 
-                className="team-logo-modal" 
-                src={`/logos/${event_details.home_team}.svg`}>
-              </Image>
-            </Row>
-            <Row className="centered-header" >
-              <h4 className="graph-title">Price Tracker</h4>
-            </Row>
-            <Row>
-              <LineChart data={event_details.daily_prices}/>
-            </Row>
-          </Container>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button block 
-            variant="secondary"
-            target="_blank"
-            href={event_details.url}>
-            {getPriceTextFromList(event_details.last_240_prices)}
-          </Button>
-        </Modal.Footer>
+        test
       </Modal>
     );
   }
