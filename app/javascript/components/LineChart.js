885fce3fd2165f423ab739c31acddd9ec1e87b11
Richard <wei.richard.s@gmail.com>
2020-02-11 18:36:55 -0800

    screw it do it dirty way

--- app/javascript/components/LineChart.js
+++ app/javascript/components/LineChart.js
@@ -1,44 +1,35 @@
 import React, { Component } from "react";
-// component and styles
-import BillboardChart from "react-billboardjs";
-import "react-billboardjs/lib/billboard.css";
+// base css
 
 class LineChart extends Component {
-  render() {
-    const data = this.props.data;
-    const times = data.map((d) => new Date(d.time).setHours(0, 0, 0, 0))
-    const prices = data.map((d) => d.price);
-    times.unshift("x");
-    prices.unshift("Price");
-    const chart_data = {
-      x: "x",
-      columns: [times,prices]
-    };
-    const axis = {
-      y: {
-        tick: {
-          format: function(x) {
-            return "$"+x;
-          }
-        }
-      },
-      x: {
-        type: "timeseries",
-        tick: {
-          format: "%e %b"
+  
+  componentDidMount() {
+    this._renderChart();
+  }
+
+  _renderChart() {
+    bb.generate({
+      bindto: "#chart",
+      data: {
+        columns: [
+          ["data1", 30, 200, 100, 170, 150, 250],
+          ["data2", 130, 100, 140, 35, 110, 50]
+        ],
+        types: {
+          data1: "line",
+          data2: "area-spline"
+        },
+        colors: {
+          data1: "red",
+          data2: "green"
         }
       }
-    };
-    const point = {
-      show: false    
-    }
-    return (
-      <BillboardChart
-        data={chart_data}
-        axis={axis}
-        point={point}
-      />
-    );
+    });
+  }
+
+  render() {
+    return <div id="chart" />;
   }
 }
+
 export default LineChart
